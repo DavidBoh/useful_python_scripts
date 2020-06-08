d760677d0ac8572b029042826d980cdd538a5ca6
David Boh <herrboh@live.com>
2020-06-08 17:29:50 -0500

    improvement and bug fix

--- createfiles.py
+++ createfiles.py
@@ -2,52 +2,55 @@
 #Author David Boh // herrboh@live.com
 import os
 
+def createpy():
+    file_ext = ".py"
+    filename = input("Please enter a name for your file: ")
+    file = open("{}{}".format(filename,file_ext), "w")
+    if file_ext == ".py":
+        file.write("#!/usr/bin/env python3\n")
+        final_name= "{}{}".format(filename,file_ext)
+        os.chmod(final_name,0o755)
+    print("\nFile {} has been created\n".format(final_name))
+    file.close()
+    creation()
 
+def createbash():
+    file_ext = ".sh"
+    filename = input("Please enter a name for your file: ")
+    file = open("{}{}".format(filename,file_ext), "w")
+    if file_ext == ".sh":
+        file.write("#!/bin/sh\n")
+        final_name= "{}{}".format(filename,file_ext)
+        os.chmod(final_name,0o755)
+    print("\nFile {} has been created\n".format(final_name))
+    file.close()
+    creation()
+    
 def creation():
     """this script creates a .py or .sh file based on user input 
     Each file will be created with execute permissions and inside of each file, 
     the first line wille include its respective shebang #! line argument"""
     
     file_ext = ""
-    bash_or_py = input("Please choose filetype\npress b for bash\npress p for pyhton\npress q to quit\n")
     active = True
     while active:
+        bash_or_py = input("\nPlease choose file type\npress b for bash\npress p for pyhton\npress q to quit\n\n")
         
-        if bash_or_py == 'b':
-            file_ext = ".sh"
-            filename = input("Please enter a name for your file ")
-            file = open("{}{}".format(filename,file_ext), "w")
-            if file_ext == ".sh":
-                file.write("#!/bin/sh\n")
-                final_name= "{}{}".format(filename,file_ext)
-                os.chmod(final_name,0o755)
-            print("File {} has been created".format(final_name))
-            file.close()
-            break
-        elif bash_or_py == 'p':
-            file_ext = ".py"
-            filename = input("Please enter a name for your file ")
-            file = open("{}{}".format(filename,file_ext), "w")
-            if file_ext == ".py":
-                file.write("#!/usr/bin/env python3\n")
-                final_name= "{}{}".format(filename,file_ext)
-                os.chmod(final_name,0o755)
-            print("File {} has been created".format(final_name))
-            file.close()
-            break
-        elif bash_or_py == 'q':
+        if bash_or_py == 'q':
             active = False
-        elif bash_or_py != 'b' or bash_or_py != 'p' or bash_or_py != 'q':
+            break
+        
+        if bash_or_py == 'p':
+            createpy()          
+            break
+
+        if bash_or_py == 'b':
+            createbash()
+            break
+
+        if bash_or_py != 'b' or bash_or_py != 'p' or bash_or_py != 'q':
             print("\nInvalid Input\n")
             creation()
-            
-            
+            break
+
 creation()
-  
-
-
-
-    
-
-    
-
