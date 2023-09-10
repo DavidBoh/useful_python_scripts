fa3838cdaefde193e4955d568568c51e4c7a9bdd
David Boh <53196387+DavidBoh@users.noreply.github.com>
2023-09-10 18:49:38 -0500

    Update pdf_to_png.py

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -1,1 +1,20 @@
+import os
+from pdf2image import convert_from_path
 
+def pdf_to_images(pdf_path, output_dir):
+  os.makedirs(output_dir, exist_ok=True)
+
+  images = convert_from_path(pdf_path)
+
+  #save each image as png
+  for i, image in enumerate(images):
+    output_path = os.path.join(output_dir, f"page_{i+1}".png")
+    image.save(output_path, "PNG")
+    print(f"Saved {output_path}")
+
+#provide PDF path and output dir
+
+pdf_path = "path/file.pdf"
+output_dir = "."
+
+pdf_to_images(pdf_path,output_dir)
